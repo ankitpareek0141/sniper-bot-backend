8a597299f252bca5ce28d57ae00dd5473ff9d298
ankitpareek0141 <ankitpareek0141@gmail.com>
2025-08-11 13:01:23 +0530

    Update: server.js

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ import { tradeLogs, addTradeLog } from './helper/tradeLogs.js';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "*",
-    // methods: ["GET", "POST", "PUT", "DELETE"],
-    // credentials: true
+    origin: "http://srv951924.hstgr.cloud:4173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
 }));
 
 // Secret for JWT
